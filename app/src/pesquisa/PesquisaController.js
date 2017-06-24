2c646ee42641e669f7f0ec3ab9f9b6fa68dfe3b8
Denilson Godinho <denilson_godinho@hotmail.com>
2017-06-24 16:27:41 -0300

    Configurações iniciais

---
+++ app/src/pesquisa/PesquisaController.js
@@ -0,0 +1,18 @@
+(function(){
+
+  angular
+       .module('app')
+       .controller('PesquisaController', [PesquisaController]);
+
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function PesquisaController( ) {
+	  var self = this;
+  }
+   
+
+})();
